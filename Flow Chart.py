d82fae72f0b9509460e273979a438a83ea2ac879
DR <dapatheai@gmail.com>
2023-03-16 11:49:37 +0700

    Task

--- Flow Chart.py
+++ Flow Chart.py
@@ -5,9 +5,26 @@ Semua sintaksis dasar bahasa pemrograman terdiri dari
 3.Perulangan  = mengulangi langkah yang sama berkali-kali selama/sampai kondisi terpenuhi
 """
 
-#sekuensial
-print('Mom said,"IIs,Go to Shop"')
-print('IIs Said,"Yes,Mom what must i do in shop?"')
-print('Mom Said,"Buy 1 bottle of milk and if they have eggs,bring 6"')
-print("Than Iis,Go to Shop")
-print("And Start to buy")
+# sequential
+print('Ibu berkata, "Pergi ke toko"')
+print('Budi Menjawab,"Baik apa yang harus saya lakukan di toko?"')
+print('Ibu Menjawab,"Beli Satu botol susu"')
+print("Maka budi berangkat ke toko")
+print("Dan Mulai berbelanja")
+
+# percabangan
+jumlah_botol_susu = 170
+jumlah_telur = 187
+print(f"jumlah botol susu {jumlah_botol_susu}botol")
+print(f"jumlah telur {jumlah_telur}butir")
+if jumlah_botol_susu > 0:
+    print("Budi mengecek harganya, dan ternyata uangnya cukup")
+    if jumlah_telur == 0:
+        print("Budi membeli 1 botol susu")
+    else:
+        print("Budi membeli 6 botol susu")
+else:
+    print("Budi tidak jadi membeli 1 botol susu")
+
+print("Budi Pulang ke rumah")
+print("Menyampaikan hasilnya kepada Ibu")
--- Task.py
+++ Task.py
@@ -5,13 +5,20 @@ tentang ibu memerintahkan anaknya untuk membeli 1 botol susu dan 6 telur
 
 #sekuensial
 print('Ibu memberi perintah"Beli 1 bottol susu dan beli 6 telur"')
-print('Anak menjawab"Oke"')
-print("Anak pergi ke toko")
-print("Apakah ada Susu")
-print("Ada")
-print("Anak Membeli Susu")
-print("Apakah ada Telur")
-print("ada")
-print("Anak Membeli 6 Telur")
-print("Menyerahkan Hasil Belanja kepada Ibu")
-print("Selesai")
+print('Budi menjawab"Oke"')
+print("Budi pergi ke toko")
+
+# percabangan
+jumlah_botol_susu = 170
+jumlah_telur = 28
+print(f"jumlah botol susu {jumlah_botol_susu} botol")
+print(f"jumlah telur {jumlah_telur} Butir")
+if jumlah_botol_susu > 0:
+    print("budi membeli susu")
+else:
+    print("Budi tidak membeli telur")
+print("Lalu")
+if jumlah_telur == 0:
+    print("Budi tidak membeli telur")
+else:
+    print("Budi membeli 6 Butir telur")
