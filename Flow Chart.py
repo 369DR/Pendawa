f34bfed079993acaf8f72af5e7da1ff6ea8a7991
DR <dapatheai@gmail.com>
2023-03-16 08:54:21 +0700

    Task

---
+++ Flow Chart.py
@@ -0,0 +1,13 @@
+"""
+Semua sintaksis dasar bahasa pemrograman terdiri dari
+1.Sekuensial  = langkah berurutan
+2.Percabangan = langkah melompat jika kondisi terpenuhi
+3.Perulangan  = mengulangi langkah yang sama berkali-kali selama/sampai kondisi terpenuhi
+"""
+
+#sekuensial
+print('Mom said,"IIs,Go to Shop"')
+print('IIs Said,"Yes,Mom what must i do in shop?"')
+print('Mom Said,"Buy 1 bottle of milk and if they have eggs,bring 6"')
+print("Than Iis,Go to Shop")
+print("And Start to buy")
---
+++ Task.py
@@ -0,0 +1,10 @@
+""""
+Tugas mengerjakan dari course,yaitu membuat flowcart dan coding
+tentang ibu memerintahkan anaknya untuk membeli 1 botol susu dan 6 telur
+"""
+
+#sekuensial
+print('Ibu memberi perintah"Beli 1 bottol susu dan beli 6 telur"')
+print('Anak menjawab"Oke"')
+print("Anak pergi ke toko")
+print("Apakah ada Susu")
